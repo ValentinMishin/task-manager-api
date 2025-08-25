d2d83b6e91f9fd9333d51cf987444b41866b18e9
Valentin <mvalentin.dev@gmail.com>
2025-08-25 19:00:27 +0700

    Added annotation for time field for proper format

--- src/main/kotlin/ru/valentin/dto/error/ErrorResponse.kt
+++ src/main/kotlin/ru/valentin/dto/error/ErrorResponse.kt
@@ -1,11 +1,12 @@
 package ru.valentin.dto.error
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
 
 data class ErrorResponse(
     val status: Int,
     val message: String,
-    val timestamp: LocalDateTime = LocalDateTime.now(),
     val details: String? = null,
-    val errors: Map<String, String>? = null
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    val timestamp: LocalDateTime = LocalDateTime.now()
 )
