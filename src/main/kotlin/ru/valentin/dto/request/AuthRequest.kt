7de0a5d79ada78a6c5da627e3610b15ceeec0c17
Valentin <mvalentin.dev@gmail.com>
2025-08-25 20:32:53 +0700

    Added swagger annotations

--- src/main/kotlin/ru/valentin/dto/request/AuthRequest.kt
+++ src/main/kotlin/ru/valentin/dto/request/AuthRequest.kt
@@ -1,14 +1,18 @@
 package ru.valentin.dto.request
 
+import io.swagger.v3.oas.annotations.media.Schema
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
 
+@Schema(description = "DTO для аутентификации или регистрации клиента API.")
 data class AuthRequest(
-    @NotBlank
+    @Schema(description = "Обязательное не пустое имя пользователя")
+    @field:NotBlank
     @Size(max = 50)
     val username: String,
 
-    @NotBlank
+    @Schema(description = "Обязательный не пустой пароль для пользователя")
+    @field:NotBlank
     @Size(max = 100)
     val password: String
 )
